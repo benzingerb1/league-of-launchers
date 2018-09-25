cc6bb3d2d6858ef05c359f4e76a4e5c333703bee
Timpy <benzingerb1@gmail.com>
2018-09-25 18:45:26 -0400

    Update league-of-launchers.bat

--- league-of-launchers.bat
+++ league-of-launchers.bat
@@ -1,5 +1,11 @@
 @echo off
 
+:: Launch Google Chrome
 cd "C:\Program Files (x86)\Google\Chrome\Application"
 start chrome.exe
 
+:: Launch League of Legends
+cd "C:\Riot Games\League of Legends"
+start LeagueClient.exe
+
+exit
